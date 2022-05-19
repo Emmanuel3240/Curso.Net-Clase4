9fc7d57f873923d163c8a8b987cfbf5eedd22e20
Emmanuel <emmanwel@gmail.com>
2022-05-18 22:55:34 -0300

    Update Program.cs
    probando switch

--- clase4/Program.cs
+++ clase4/Program.cs
@@ -1,14 +1,39 @@
-﻿Console.WriteLine("Ingrese su edad: ");
-int edad = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Ingrese el primer numero");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+int num2 = int.Parse(Console.ReadLine());
 
-Console.WriteLine("La edad ingresada es: " + edad);
+Console.WriteLine("1 - Suma");
+Console.WriteLine("2 - Resta");
+Console.WriteLine("3 - Multiplicación");
+Console.WriteLine("4 - División");
 
-if (edad >= 18)
+Console.WriteLine("Introduce una opción: ");
+string opcion = Console.ReadLine();
+
+double resultado = 0;
+
+switch (opcion)
 {
-    Console.WriteLine("Usted es mayor de edad");
-}
-else
-{
-    Console.WriteLine("Usted es menor de edad");
+    case "1":
+        Console.WriteLine("Suma");
+        resultado = num1 + num2;
+        break;
+    case "2":
+        Console.WriteLine("Resta");
+        resultado = num1 - num2;
+        break;
+    case "3":
+        Console.WriteLine("Multiplicación");
+        resultado = num1 * num2;
+        break;
+    case "4" when num2 != 0:
+        Console.WriteLine("División");
+        resultado = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Opción incorrecta");
+        break;
 }
 
+Console.WriteLine($"El resultado es: {resultado}");
